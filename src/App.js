c356e8fdd9a9538329ced79c7182b6b3326c1e94
Estefania Loewenberg <estloewenberg@gmail.com>
2018-04-15 09:51:02 +0200

    added basename browserrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./styles/App.css";
 import Navigation from "./navigation";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import MainInfo from "./mainInfo";
 import Contact from "./contact";
 import PersonalProjects from "./personalProjects";
@@ -10,15 +10,15 @@ import About from "./about";
 class App extends Component {
     render() {
         return (
-            <BrowserRouter>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <div>
                     <Navigation />
-                    <Switch>
+
                         <Route exact path="/" component={MainInfo} />
                         <Route path="/contact" component={Contact} />
                         <Route path="/about" component={About} />
                         <Route path="/projects" component={PersonalProjects} />
-                    </Switch>
+
                 </div>
             </BrowserRouter>
         );
